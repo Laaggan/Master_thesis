06c470da48cb112094641cc97a6a90fc24521e55
Linus Lagergren <lagergrenlinus@gmail.com>
2019-12-06 10:27:35 +0100

    Rewrote code for data augmentation so it hopefully runs faster

--- 20191204-unet_with_data_augmentation.py
+++ 20191204-unet_with_data_augmentation.py
@@ -12,10 +12,11 @@ metrics = [dice, dice_en_metric, dice_core_metric, dice_whole_metric, 'accuracy'
 
 train_ind, val_ind = create_train_test_split()
 
-batch_size=64
-lr=1e-4
+batch_size = 16
+lr = 1e-4
 epochs = 100
 total_num_slices = 1.5e4
+seed = 1
 num_batches_in_epoch = int(total_num_slices // batch_size)
 
 # Setup the model
@@ -25,73 +26,43 @@ train_choice = np.random.choice(train_ind, len(val_ind))
 X_train, Y_train = load_patients_numpy("data_numpy_separate_patients_original_size", train_choice, cropping=True)
 X_val, Y_val = load_patients_numpy("data_numpy_separate_patients_original_size", val_ind, cropping=True)
 
-im_gen = ImageDataGenerator(
-   rotation_range=20,
-   horizontal_flip=True,
-   vertical_flip=True,
-   width_shift_range=0.1,
-   height_shift_range=0.1,
-   shear_range=2,
-   zoom_range=0.1)
+train_datagen = ImageDataGenerator(
+    rotation_range=20,
+    horizontal_flip=True,
+    vertical_flip=True,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.2,
+    zoom_range=0.1)
 
-import sys
+input_generator = train_datagen.flow(
+        X_train,
+        batch_size=batch_size,
+        seed=seed
+)
 
-def create_a_batch(aug_iter, batch_size):
-    aug_data = [next(aug_iter)[0].astype(np.float32) for i in range(batch_size)]
-    X = np.zeros((batch_size, 176, 176, 4))
-    Y = np.zeros((batch_size, 176, 176, 4))
+label_generator = train_datagen.flow(
+        Y_train,
+        batch_size=batch_size,
+        seed=seed
+)
 
-    for i in range(batch_size):
-        X[i, :, :, :] = aug_data[i][:, :, 0:4].reshape((1, 176, 176, 4))
-        Y[i, :, :, :] = aug_data[i][:, :, 4:8].reshape((1, 176, 176, 4))
-    Y = np.round(Y)
-    return X, Y
-
-training_results = []
-validation_results = []
-patients_in_aug_iter = 10
-num_batches = int((patients_in_aug_iter*66)//batch_size)
-
+# Where to save logs and weights
 weights_path = datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + '-all-lr-' + str(lr)\
-               + '-n-' + str(X_train.shape[0]) + "-weights_he_normal_l2_0.001-data_augmentation.hdf5"
-train_res_path = datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + 'train_results.pkl'
-val_res_path = datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + 'validation_results.pkl'
+               + '-n-' + str(X_train.shape[0]) + "-weights_data_aug_fit_gen.hdf5"
+log_dir = "logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "-all" \
+          + '-lr-' + str(lr) + '-n-' + str(X_train.shape[0]) + '_data_aug_fit_gen'
 
-for epoch in range(epochs):
-    print("Epoch", epoch)
-    for _ in range(int(len(train_ind)//patients_in_aug_iter)):
-        curr_ind = np.random.choice(train_ind, patients_in_aug_iter)
-        print("Patients:", curr_ind)
-        X_aug, Y_aug = load_patients_numpy("data_numpy_separate_patients_original_size", curr_ind, cropping=True)
-        data = np.concatenate((X_aug, Y_aug), axis=3)
-        aug_iter = im_gen.flow(data)
-        for batch_id in range(num_batches):
-            sys.stdout.write('\r')
-            # the exact output you're looking for:
-            i = round(((batch_id+1)*batch_size))
-            sys.stdout.write("Sample " + str(i))
-            sys.stdout.flush()
+cp = ModelCheckpoint(weights_path, save_best_only=True, monitor='val_loss', mode='auto', verbose=1, period=1)
+es = EarlyStopping(monitor='val_loss', mode='auto', verbose=1, patience=10)
+tbc = TensorBoard(log_dir=log_dir)
 
-            X_batch, Y_batch = create_a_batch(aug_iter, batch_size)
-            loss = unet.train_on_batch(X_batch, Y_batch)
+tot_generator = zip(input_generator, label_generator)
 
-    train_eval = unet.evaluate(X_train, Y_train)
-    #[dice, dice_en_metric, dice_core_metric, dice_whole_metric, 'accuracy']
-    s_train = "Train; Loss:{}, DSC:{}, DSC enhancing:{}, DSC core:{}, DSC whole:{}, accuracy:{}"
-    # [dice, dice_en_metric, dice_core_metric, dice_whole_metric, 'accuracy']
-    print(s_train.format(*train_eval))
-    training_results.append(train_eval)
-
-    val_eval = unet.evaluate(X_val, Y_val)
-    s_val = "Validation; Loss:{}, DSC:{}, DSC enhancing:{}, DSC core:{}, DSC whole:{}, accuracy:{}"
-    # [dice, dice_en_metric, dice_core_metric, dice_whole_metric, 'accuracy']
-    print(s_val.format(*val_eval))
-    validation_results.append(val_eval)
-
-    with open(train_res_path, 'wb') as f:
-        pickle.dump(training_results, f)
-
-    with open(val_res_path, 'wb') as f:
-        pickle.dump(validation_results, f)
-
-    unet.save_weights(weights_path)
+unet.fit_generator(
+        tot_generator,
+        steps_per_epoch=num_batches_in_epoch,
+        epochs=100,
+        validation_data=(X_val, Y_val),
+        callbacks=[cp, es, tbc]
+)
