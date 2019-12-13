44e0546d903ff0d1bdb02a53b93cb922ae5f7f3b
Linus Lagergren <lagergrenlinus@gmail.com>
2019-12-13 18:01:18 +0100

    Changed loss function

--- 20191212-trying_skip_connections.py
+++ 20191212-trying_skip_connections.py
@@ -20,7 +20,7 @@ seed = 1
 num_batches_in_epoch = int(total_num_slices // batch_size)
 
 # Setup the model
-unet = unet_res(input_size=input_size, num_classes=4, lr=lr, loss=weighted_log_loss, metrics=metrics)
+unet = unet_res(input_size=input_size, num_classes=4, lr=lr, loss='categorical_crossentropy', metrics=metrics)
 
 X_train, Y_train = load_patients_numpy("data_numpy_separate_patients_original_size", train_ind, cropping=True)
 X_val, Y_val = load_patients_numpy("data_numpy_separate_patients_original_size", val_ind, cropping=True)
