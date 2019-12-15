311b33ca1068098cf188a7b7392df31b2a4c9bb1
Linus Lagergren <lagergrenlinus@gmail.com>
2019-12-15 17:41:09 +0100

    Added weird slicing which seems to make it not crash when working with the full dataset

--- 20191215-unet_for_asgeir.py
+++ 20191215-unet_for_asgeir.py
@@ -22,8 +22,8 @@ num_batches_in_epoch = int(total_num_slices // batch_size)
 # Setup the model
 unet = unet_dong_et_al2(input_size=input_size, num_classes=2, lr=lr, loss='categorical_crossentropy', metrics=metrics)
 
-X_train, Y_train = load_patients_numpy("data_numpy_separate_patients_original_size", train_ind, cropping=True)
-X_val, Y_val = load_patients_numpy("data_numpy_separate_patients_original_size", val_ind, cropping=True)
+X_train, Y_train = load_patients_numpy("data_numpy_separate_patients_original_size", train_ind[0:], cropping=True)
+X_val, Y_val = load_patients_numpy("data_numpy_separate_patients_original_size", val_ind[0:], cropping=True)
 
 # Convert one hot encoded labels to binary classification task
 Y_train = convert_brats_to_asgeir(Y_train)
